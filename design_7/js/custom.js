1798b3681bbbf7eb6c91f1c1447cde263df6cb46
Tadhani Lalji <tadhani.lalji@gmail.com>
2023-08-02 04:18:55 -0700

    added project

---
+++ design_7/js/custom.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("#sidebarToggle").on('click', function (e) {
+        $(this).toggleClass('active');
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+            //$('.sidebar .collapse').collapse('hide');
+        };
+    });
+    $(".custom-scroll").mCustomScrollbar({
+        theme: "dark"
+    });
+    $('.main-navigation').find('li ul').parent().addClass('has-sub');
+})
